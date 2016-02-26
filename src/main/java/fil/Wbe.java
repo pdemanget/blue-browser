fd4f9c0f87e041578c16c9c6032e665a4563c209
Philippe Demanget <philippe.demanget@viseo.com>
2016-02-26 07:45:10 +0100

    TextField

--- src/main/java/fil/Wbe.java
+++ src/main/java/fil/Wbe.java
@@ -2,15 +2,16 @@ package fil;
 
 import java.net.URL;
 
-import netscape.javascript.JSObject;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.ButtonBase;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -18,12 +19,14 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import netscape.javascript.JSObject;
 /**
  * http://stackoverflow.com/questions/17522343/custom-javafx-webview-protocol-handler
  * http://stackoverflow.com/questions/16215844/javafx-webview-disable-same-origin-policy-allow-cross-domain-requests
  * @author pdemanget
  *
  */
+@SuppressWarnings("restriction")
 public class Wbe extends Application {
 
 	private static final String INDEX = "myapp://resource/index.html";
@@ -50,6 +53,7 @@ public class Wbe extends Application {
 		final WebView browser = new WebView();
 		browser.setPrefHeight(1600);
 
+		@SuppressWarnings("restriction")
 		final WebEngine webEngine = browser.getEngine();
 		Hyperlink hpl = new Hyperlink("www.google.com");
 		hpl.setOnAction(new EventHandler<ActionEvent>() {
@@ -62,7 +66,13 @@ public class Wbe extends Application {
 		MenuButton menuButton = new MenuButton("go", null,getMenuItem(webEngine,"google","www.google.com")
 				,getMenuItem(webEngine,"local","http://localhost:8001"));
 		menuButton.setOnAction((e)->{webEngine.load("http://www.google.com");});
-		root.getChildren().addAll( menuButton, browser);
+		
+		TextField text = new TextField();
+		text.textProperty().addListener((obs,old,value)->{
+			System.out.println(value);
+			webEngine.load(value);
+		});
+		root.getChildren().addAll( menuButton, text, browser);
 		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
 		//webEngine.load("http://localhost:8001/index.html");
 		//webEngine.loadContent("<html><head><title>IWH</title></head><body><h1>test</h1><script>alert('toto');</script></body></html>");
